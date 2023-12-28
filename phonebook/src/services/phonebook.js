9c194afd7836fcafbd1060b45163c2ce98dfa67c
[Zaharaddeen Balarabe] <[zaharaddeenb@gmail.com]>
2023-12-29 00:30:25 +0100

    3.11 phonebook full stack

--- phonebook/src/services/phonebook.js
+++ phonebook/src/services/phonebook.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3002/api/persons'
+const baseUrl = '/api/persons'
 
 const getContacts = async () => {
     const request = axios.get(baseUrl)
